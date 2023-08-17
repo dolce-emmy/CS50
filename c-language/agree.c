8123791330e7bf7b23bc4965ca46fda2cb096988
Eman Yassin <eman.yassin83@gmail.com>
2023-08-17 14:53:11 +0200

    or operator

--- c-language/agree.c
+++ c-language/agree.c
@@ -6,9 +6,9 @@ int main (void)
 {
   char c =  get_char("do you agree? ");
 
-  if (c == 'y') {
+  if (c == 'y' || c == 'Y') {
     printf("agreed \n");
-  } else if (c == 'n') {
+  } else if (c == 'n' || c == 'N') {
     printf("not agreed \n");
     } else {
         printf("invalid input \n");
