c46949d7ae575274824ce3c22d3d2c50103452d8
Eman Yassin <eman.yassin83@gmail.com>
2023-08-18 11:39:56 +0200

    mario paradigm

---
+++ c-language/mario.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main (void) 
+
+{
+
+// here we are giving const to n so that it can not be changed later on in the code
+
+ const int n = 3;
+
+ for(int i = 0; i < n; i++) 
+{
+    for(int j = 0; j< n; j ++)
+    {
+        printf("#");
+    }
+
+    printf("\n");
+}
+
+    // here we print \n out of the loop so that it only prints once allowing the ? to be next to each other
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     printf("?");
+    // }
+    // printf("\n");
+
+// for (int i = 0; i< 3 ; i++)
+
+// {
+
+//     printf("#\n");
+// }
+
+}
